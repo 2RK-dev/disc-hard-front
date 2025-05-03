cf6d8c4fc6b7cc9c001940497861c1cc2bb81e08
Ryan <ryanlai2486@gmail.com>
2025-05-03 15:55:11 +0300

    Add MemberUtils.ts and update member, salon, and user services

--- src/services/salon.ts
+++ src/services/salon.ts
@@ -4,6 +4,7 @@ import data from "@/test/serverData.json";
 import { Member } from "@/type/Member";
 import { Message } from "@/type/Message";
 import { Server } from "@/type/Server";
+import { User } from "@/type/User";
 
 const salons = data as Server[];
 
@@ -25,15 +26,65 @@ export async function sendMessage(
 	salonId: number,
 	user: Member,
 	message: string
-): Promise<Message> {
-	//genera number
+): Promise<Message[]> {
 	const id = Math.floor(Math.random() * 1000000);
-
 	const newMessage: Message = {
 		id: id,
 		textContent: message,
 		author: user,
 		timestamp: new Date().toISOString(),
 	};
-	return newMessage;
+	const salon = salons.find((salon) => salon.id === salonId);
+	if (!salon) {
+		throw new Error(`Salon with id ${salonId} not found`);
+	}
+	salon.messages.push(newMessage);
+	return salon.messages;
+}
+
+export async function addSalon(
+	salonName: string,
+	description: string,
+	creator: User
+): Promise<Server> {
+	const creatorInMember: Member = {
+		id: creator.id,
+		alias: creator.name,
+		role: "owner",
+		user: creator,
+	};
+
+	const newSalon: Server = {
+		id: (await getLastSalonId()) + 1,
+		description: description,
+		name: salonName,
+		members: [creatorInMember],
+		messages: [],
+	};
+
+	salons.push(newSalon);
+
+	return newSalon;
+}
+
+export async function getLastSalonId(): Promise<number> {
+	const lastSalon = salons[salons.length - 1];
+	if (!lastSalon) {
+		throw new Error("No salons found");
+	}
+
+	return lastSalon.id;
+}
+
+export async function addMemberToSalon(
+	salonId: number,
+	member: Member
+): Promise<Server | null> {
+	const salon = salons.find((salon) => salon.id === salonId);
+	if (!salon) {
+		throw new Error(`Salon with id ${salonId} not found`);
+	}
+	salon.members.push(member);
+	console.log("salon", salon.members);
+	return salon;
 }
