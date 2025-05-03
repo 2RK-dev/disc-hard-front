78b13c2bba8a6be0334981a2159ea03420bf232e
Ryan <ryanlai2486@gmail.com>
2025-05-03 22:12:10 +0300

    Refactor layout components and remove ServerProvider from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { ServerProvider } from "@/test/server-context";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +26,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<ServerProvider>{children}</ServerProvider>
+				{children}
 			</body>
 		</html>
 	);
