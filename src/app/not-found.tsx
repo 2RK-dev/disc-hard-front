23eb8c4ba57497844004c2c088188ad8c0d2ba34
Ryan <ryanlai2486@gmail.com>
2025-06-27 23:53:37 +0300

    Refactor loading and not-found components to enhance code clarity by removing unnecessary comments

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,11 +7,10 @@ import {generateStars} from "@/components/Star";
 import {useState} from "react";
 
 export default function NotFound() {
-    const [stars] = useState(() => generateStars(100)); // Générer 100 étoiles
+    const [stars] = useState(() => generateStars(100));
 
     return (
         <div className="relative flex h-screen w-full items-center justify-center bg-[#313338] overflow-hidden">
-            {/* Étoiles */}
             {stars}
 
             <div className="relative flex flex-col items-center justify-center text-center">
@@ -22,8 +21,6 @@ export default function NotFound() {
                         width={200}
                         height={200}></Image>
                 </div>
-
-                {/* Texte d'erreur */}
                 <h1 className="mb-2 text-4xl font-bold text-white">404</h1>
                 <h2 className="mb-6 text-2xl font-semibold text-white">
                     Astronaute perdu dans l&apos;espace
@@ -32,8 +29,6 @@ export default function NotFound() {
                     On dirait que vous avez dérivé trop loin. Cette page n&apos;existe pas
                     ou a été déplacée vers une autre galaxie.
                 </p>
-
-                {/* Bouton de retour */}
                 <Button asChild className="bg-blue-600 hover:bg-blue-700">
                     <Link href="/">Retour à la Terre</Link>
                 </Button>
