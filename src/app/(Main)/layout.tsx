05717ed99cf8dfa6dca316268e043edf53e3837a
Ryan <ryanlai2486@gmail.com>
2025-06-28 20:47:15 +0300

    Implement user authentication state management in layout and user store persistant

--- src/app/(Main)/layout.tsx
+++ src/app/(Main)/layout.tsx
@@ -2,38 +2,40 @@
 
 import type React from "react";
 
-import { SalonSidebar } from "@/components/salon-sidebar";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { UserDropdownMenu } from "./user-dropdown-menu";
+import {SalonSidebar} from "@/components/salon-sidebar";
+import {useRouter} from "next/navigation";
+import {UserDropdownMenu} from "./user-dropdown-menu";
+import {useCurrentUserStore} from "@/contexts/userStore";
 
 export default function HomeLayout({
-	children,
-}: {
-	children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-	const router = useRouter();
+    const router = useRouter();
+    const currentUser = useCurrentUserStore((s) => s.currentUser);
+	const hasHydrated = useCurrentUserStore((s) => s.hasHydrated);
 
-	useEffect(() => {
-		const isAuthenticated =
-			sessionStorage.getItem("isAuthenticated") === "true";
 
-		if (!isAuthenticated) {
-			sessionStorage.setItem("isAuthenticated", "true");
-		}
-	}, [router]);
+    if (!currentUser && hasHydrated) {
+        router.replace("/login");
+    }
+    if (!hasHydrated) {
+		return null;
+	}
 
-	return (
-		<div className="flex h-screen bg-[#1e1f22] text-white overflow-hidden">
-			<SalonSidebar />
 
-			<div className="flex flex-col flex-1">
-				<div className="flex-1 min-h-0 flex">{children}</div>
-				<div className="h-[52px] bg-[#232428] px-2 flex items-center">
-					<UserDropdownMenu />
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex h-screen bg-[#1e1f22] text-white overflow-hidden">
+            <SalonSidebar/>
+
+            <div className="flex flex-col flex-1">
+                <div className="flex-1 min-h-0 flex">{children}</div>
+                <div className="h-[52px] bg-[#232428] px-2 flex items-center">
+                    <UserDropdownMenu/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
--- src/contexts/userStore.tsx
+++ src/contexts/userStore.tsx
@@ -1,12 +1,27 @@
 import { User } from "@/type/User";
 import { create } from "zustand";
+import {persist} from "zustand/middleware";
 
 interface CurrentUserState {
 	currentUser: User | null;
 	setCurrentUser: (user: User | null) => void;
+    hasHydrated: boolean;
+    setHasHydrated: (state: boolean) => void;
 }
 
-export const useCurrentUserStore = create<CurrentUserState>((set) => ({
-	currentUser: null,
-	setCurrentUser: (user) => set({ currentUser: user }),
-}));
+export const useCurrentUserStore = create<CurrentUserState>()(
+    persist(
+        (set) => ({
+            currentUser: null,
+            hasHydrated: false,
+            setCurrentUser: (user) => set({ currentUser: user }),
+            setHasHydrated: (state) => set({ hasHydrated: state }),
+        }),
+        {
+            name: 'current-user-store',
+            onRehydrateStorage: () => (state) => {
+                state?.setHasHydrated(true);
+            },
+        }
+    )
+);
